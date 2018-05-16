fb8c8f64dd9e493181eecb525dff4b85d0e2bd79
Mostafa Zare <mostafazare@gmail.com>
2018-05-16 17:02:43 +0430

    add mysql engine

--- IBSngReport/settings.py
+++ IBSngReport/settings.py
@@ -83,12 +83,23 @@ WSGI_APPLICATION = 'IBSngReport.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+ }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': os.path.join(CONFIG_DIR,'database.ini'),
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         },
+#     }
+# }
 
 
 # Password validation
